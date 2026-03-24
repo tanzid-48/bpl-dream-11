e5cda9e4507890234a67f6aa42820b4680bf95d2
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-24 21:47:16 +0600

    Add the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
 
 import './App.css'
+import NavBar from './componete/NavBar'
 
 function App() {
-  
+
 
   return (
     <>
+  <nav>
+    <NavBar></NavBar>
+  </nav>
+
      
-      
-        <div>
-          <h1 className='text-red-500'>Get started</h1>
-          <button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-info">Info</button>
-<button className="btn btn-success">Success</button>
-<button className="btn btn-warning">Warning</button>
-<button className="btn btn-error">Error</button>
-          
-        </div>
-    
-
-    
-
-    
 
     </>
   )
---
+++ src/componete/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import logo from "../../assets/logo.png"
+import dollarImg from "../../assets/DoubleDollar.png";
+
+const NavBar = () => {
+    return (
+
+
+        <div className="navbar w-10/12 mx-auto bg-base-100 mt-3  ">
+            <div className="flex-1">
+                <img src={logo} alt="logo" className="w-20" />
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li className='paragraph mr-4'><a>Home</a></li>
+                    <li className='paragraph mr-4'><a>Fixture</a></li>
+                    <li className='paragraph mr-4'><a>Teams</a></li>
+                    <li className='paragraph mr-4'><a>Schedules</a></li>
+                    <li>
+                        <button className="btn btn-soft font-bold py-3 flex items-center gap-2">
+                            0 Coin
+                            <img src={dollarImg} alt="coin" className="w-5 h-5" />
+                        </button>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+
+    );
+};
+
+export default NavBar;
