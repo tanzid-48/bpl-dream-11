d0639e7f8849f0234a0cd4cd76bc6baed40a73d0
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-24 16:53:06 +0600

    BPL Dream project setup and include Tailwind and daisiUI

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      
+        <div>
+          <h1 className='text-red-500'>Get started</h1>
+          <button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
+          
+        </div>
+    
+
+    
+
+    
+
+    </>
+  )
+}
+
+export default App
