345eac9125985447c41f358d94300847094a7f74
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-25 09:47:32 +0600

    State Management & Toggle Functionality of available and selected player

--- src/componete/player/Player.jsx
+++ src/componete/player/Player.jsx
@@ -1,20 +1,40 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvaliablePlayer from '../avaliablePlayer/AvaliablePlayer';
 
-const Player = ({playerPromise}) => {
-    const playerContent = use(playerPromise);
-    
-    return (
-      
-         <div className="">
-            Player : {playerContent.length}
+const Player = ({ playerPromise }) => {
+     const playerContent = use(playerPromise);
 
-    <AvaliablePlayer playerContent = {playerContent}></AvaliablePlayer>
 
-         </div>   
+      const [selectedType,setSelectedType] = useState("available")
 
-       
-    );
+     return (
+
+          <div className=" container mx-auto my-14">
+            
+            <div className="flex justify-between items-center w-10/12 mx-auto mb-6">
+               <h2 className='font-bold text-2xl'>Available player</h2>
+
+               <div className="">
+           <button
+           onClick={() => setSelectedType ("available")}
+            className={`btn ${selectedType === "available" ? "bg-[#E7FE29]" : ""} rounded-r-none rounded-l-xl`}>Available</button>
+
+           <button
+              onClick={() => setSelectedType ("selected")}
+           className={`btn ${selectedType === "selected" ? 'bg-[#E7FE29]' : ''} rounded-l-none rounded-r-xl`}>Selected</button>
+
+               </div>
+            </div>
+
+
+
+
+               <AvaliablePlayer playerContent={playerContent}></AvaliablePlayer>
+
+          </div>
+
+
+     );
 };
 
 export default Player;
